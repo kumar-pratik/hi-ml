f990a3834114ae93b7ea2caff941982d897aa7d8
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-03-10 17:17:15 +0000

    Add method to get item ID from folder and file names

--- hi-ml-histopathology/src/histopathology/utils/girder.py
+++ hi-ml-histopathology/src/histopathology/utils/girder.py
@@ -8,6 +8,7 @@ from typing import List, Optional
 from dataclasses import dataclass, astuple, asdict
 
 import numpy as np
+import pandas as pd
 from girder_client import GirderClient
 
 
@@ -41,6 +42,7 @@ class Transparent(Color):
 
 @dataclass
 class Element:
+    label: str
     fill_color: Color
     line_color: Color
 
@@ -57,38 +59,71 @@ class Coordinates:
         except ValueError as e:
             raise TypeError(f"Error converting coordinates to float: \"{asdict(self)}\"") from e
 
+    def __array__(self):
+        return np.asarray(astuple(self))
+
 
 @dataclass
 class Rectangle(Element):
-    center: Coordinates
+    top_left: Coordinates
     width: float
     height: float
 
     def __post_init__(self):
-        if not isinstance(self.center, Coordinates):
-            raise TypeError(f"Center must be an instance of Coordinates, not {type(self.center)}")
+        if not isinstance(self.top_left, Coordinates):
+            raise TypeError(f"Points must be instances of Coordinates, not {type(self.top_left)}")
         if self.width < 0:
             raise ValueError(f"Width must be a positive number, but \"{self.width}\" was passed")
         if self.height < 0:
             raise ValueError(f"Height must be a positive number, but \"{self.height}\" was passed")
+
+    @property
+    def center(self):
+        size = np.array((self.width, self.height))
+        return np.asarray(self.top_left) + size / 2
 
     def as_json(self):
         data = {}
         data["fillColor"] = str(self.fill_color)
         data["lineColor"] = str(self.line_color)
         data["type"] = "rectangle"
-        data["center"] = list(astuple(self.center))
-        data["center"].append(0)  # the 3rd dimension is enforced by the JSON schema
-        data["width"] = self.width
-        data["height"] = self.height
+        data["center"] = self.center.tolist() + [0]
+        data["width"] = float(self.width)
+        data["height"] = float(self.height)
+        data["label"] = {"value": self.label}
+        return data
+
+
+@dataclass
+class Point(Element):
+    center: Coordinates
+
+    def __post_init__(self):
+        if not isinstance(self.center, Coordinates):
+            raise TypeError(f"Center must be an instance of Coordinates, not {type(self.center)}")
+
+    def as_json(self):
+        data = {}
+        data["fillColor"] = str(self.fill_color)
+        data["lineColor"] = str(self.line_color)
+        data["type"] = "point"
+        data["center"] = list(astuple(self.center)) + [0]
+        data["label"] = {"value": self.label}
         return data
 
 
 @dataclass
 class Annotation:
+    name: str
     elements: Optional[List[Element]] = None
-    name: str = ""
     description: str = ""
+
+    def __post_init__(self):
+        if not self.name:
+            # This is enforced by the JSON schema
+            raise ValueError('The annotation name cannot be empty')
+        if self.elements is None:
+            self.elements = []
 
     def as_json(self):
         data = {}
@@ -119,3 +154,13 @@ class DigitalSlideArchive:
             json=annotation.as_json(),
         )
         return response
+
+    def get_file_id(self, folder_name, file_name):
+        folders = self._client.get('folder', {'text': folder_name})
+        assert len(folders) == 1
+        folder_id = folders[0]['_id']
+        iterator = self._client.listItem(folder_id, name=file_name)
+        matches = list(iterator)
+        assert matches, f'File {file_name} not found in folder {folder_name}'
+        assert len(matches) == 1
+        return matches[0]['_id']
