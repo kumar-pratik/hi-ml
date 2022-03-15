e775e174c63f968fe4581b7fc27d5920a0aadad6
Kenza <t-kbouzid@microsoft.com>
2022-03-15 17:38:45 +0000

    add proper logging

--- hi-ml-histopathology/src/histopathology/configs/classification/custom_subset/dump_dataset_csv.py
+++ hi-ml-histopathology/src/histopathology/configs/classification/custom_subset/dump_dataset_csv.py
@@ -5,6 +5,10 @@
 import argparse
 import pandas as pd
 import json
+import logging
+from health_azure.utils import is_local_rank_zero
+
+from health_ml.utils.common_utils import logging_to_stdout
 
 
 def dump_dataset_csv_from_wsi_ids(datalist_json: str, dataset_csv: str, dest_dataset_csv: str) -> None:
@@ -23,9 +27,10 @@ def dump_dataset_csv_from_wsi_ids(datalist_json: str, dataset_csv: str, dest_dat
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    logging_to_stdout("INFO" if is_local_rank_zero() else "ERROR")
     parser.add_argument("--datalist-json", type=str, default="panda/datalist_20.json")
     parser.add_argument("--dataset-csv", type=str, default="panda/dataset_20.csv")
     parser.add_argument("--dest-dataset-csv", type=str, default="panda/dataset_20.csv")
     args = parser.parse_args()
-    print(f"Selecting target WSI ids from {args.datalist_json}  and wrinting them to {args.dest_dataset_csv}")
+    logging.info(f"Selecting target WSI ids from {args.datalist_json}  and wrinting them to {args.dest_dataset_csv}")
     dump_dataset_csv_from_wsi_ids(args.datalist_json, args.dataset_csv, args.dest_dataset_csv)
