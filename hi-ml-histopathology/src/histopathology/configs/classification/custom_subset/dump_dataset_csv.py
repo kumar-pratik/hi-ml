efdab988552d7e7860b11c7402abea940e48c0c2
Kenza <t-kbouzid@microsoft.com>
2022-03-15 15:16:12 +0000

    dump target wsi in csv

---
+++ hi-ml-histopathology/src/histopathology/configs/classification/custom_subset/dump_dataset_csv.py
@@ -0,0 +1,31 @@
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
+import argparse
+import pandas as pd
+import json
+
+
+def dump_dataset_csv_from_wsi_ids(datalist_json: str, dataset_csv: str, dest_dataset_csv: str) -> None:
+
+    # get target wsi ids
+    with open(datalist_json, "r") as f:
+        slides = json.load(f)
+    wsi_ids = [slide['image'].split(".")[0] for slide in slides["training"]]
+    wsi_ids
+
+    # select corresponding tiles from dataset
+    df = pd.read_csv(dataset_csv)
+    df.to_csv(dataset_csv, index=False)
+    df.loc[df['slide_id'].isin(wsi_ids)].to_csv(dest_dataset_csv, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--datalist-json", type=str, default="panda/datalist_20.json")
+    parser.add_argument("--dataset-csv", type=str, default="panda/dataset_20.csv")
+    parser.add_argument("--dest-dataset-csv", type=str, default="panda/dataset_20.csv")
+    args = parser.parse_args()
+    print(f"Selecting target WSI ids from {args.datalist_json}  and wrinting them to {args.dest_dataset_csv}")
+    dump_dataset_csv_from_wsi_ids(args.datalist_json, args.dataset_csv, args.dest_dataset_csv)
