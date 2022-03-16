e89359da9f109bd735d5077ed719727db403106d
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-03-16 12:23:24 +0000

    Add some typing hints

--- hi-ml-histopathology/src/histopathology/utils/girder.py
+++ hi-ml-histopathology/src/histopathology/utils/girder.py
@@ -4,12 +4,16 @@
 #  ------------------------------------------------------------------------------------------
 
 import os
-from typing import List, Optional
 from dataclasses import dataclass, astuple, asdict
+from typing import Dict, List, Optional, Sequence, Union
 
 import numpy as np
-import pandas as pd
 from girder_client import GirderClient
+
+
+TypeRectangleJSON = Dict[str, Union[str, float, Dict[str, str]]]
+TypePointJSON = Dict[str, Union[str, List[float], Dict[str, str]]]
+TypeAnnotationJSON = Dict[str, Union[str, List[Dict]]]
 
 
 @dataclass
@@ -19,24 +23,24 @@ class Color:
     blue: int
     alpha: int
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         array = np.asarray(self)
         if (array < 0).any() or (array > 255).any():
             raise ValueError(f"All RGBA components must be between 0 and 255, but {astuple(self)} were passed")
 
-    def __array__(self):
+    def __array__(self) -> np.ndarray:
         return np.array(self.components, dtype=int)
 
     @property
-    def components(self):
+    def components(self) -> Sequence[int]:
         return astuple(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"rgba{astuple(self)}"
 
 
 class Transparent(Color):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(0, 0, 0, 0)
 
 
@@ -46,20 +50,23 @@ class Element:
     fill_color: Color
     line_color: Color
 
+    def as_json(self) -> Dict:
+        raise NotImplementedError
+
 
 @dataclass
 class Coordinates:
     x: float
     y: float
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         try:
             float(self.x)
             float(self.y)
         except ValueError as e:
             raise TypeError(f"Error converting coordinates to float: \"{asdict(self)}\"") from e
 
-    def __array__(self):
+    def __array__(self) -> np.ndarray:
         return np.asarray(astuple(self))
 
 
@@ -69,7 +76,7 @@ class Rectangle(Element):
     width: float
     height: float
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if not isinstance(self.top_left, Coordinates):
             raise TypeError(f"Points must be instances of Coordinates, not {type(self.top_left)}")
         if self.width < 0:
@@ -78,12 +85,12 @@ class Rectangle(Element):
             raise ValueError(f"Height must be a positive number, but \"{self.height}\" was passed")
 
     @property
-    def center(self):
+    def center(self) -> np.ndarray:
         size = np.array((self.width, self.height))
         return np.asarray(self.top_left) + size / 2
 
-    def as_json(self):
-        data = {}
+    def as_json(self) -> TypeRectangleJSON:
+        data: TypeRectangleJSON = {}
         data["fillColor"] = str(self.fill_color)
         data["lineColor"] = str(self.line_color)
         data["type"] = "rectangle"
@@ -98,12 +105,12 @@ class Rectangle(Element):
 class Point(Element):
     center: Coordinates
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if not isinstance(self.center, Coordinates):
             raise TypeError(f"Center must be an instance of Coordinates, not {type(self.center)}")
 
-    def as_json(self):
-        data = {}
+    def as_json(self) -> TypePointJSON:
+        data: TypePointJSON = {}
         data["fillColor"] = str(self.fill_color)
         data["lineColor"] = str(self.line_color)
         data["type"] = "point"
@@ -118,15 +125,15 @@ class Annotation:
     elements: Optional[List[Element]] = None
     description: str = ""
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if not self.name:
             # This is enforced by the JSON schema
             raise ValueError('The annotation name cannot be empty')
         if self.elements is None:
             self.elements = []
 
-    def as_json(self):
-        data = {}
+    def as_json(self) -> TypeAnnotationJSON:
+        data: TypeAnnotationJSON = {}
         data["name"] = self.name
         data["description"] = self.description
         data["elements"] = [element.as_json() for element in self.elements]
@@ -138,7 +145,7 @@ class DigitalSlideArchive:
             self,
             api_url: Optional[str] = None,
             api_key: Optional[str] = None,
-            ):
+    ):
         self._client = self._get_client(api_url, api_key)
 
     @staticmethod
@@ -147,7 +154,7 @@ class DigitalSlideArchive:
         client.authenticate(apiKey=os.environ["GIRDER_API_KEY"] if api_key is None else api_key)
         return client
 
-    def add_annotation(self, item_id: str, annotation: Annotation):
+    def add_annotation(self, item_id: str, annotation: Annotation) -> Dict:
         response = self._client.post(
             path="annotation",
             parameters={"itemId": item_id},
@@ -155,9 +162,9 @@ class DigitalSlideArchive:
         )
         return response
 
-    def get_file_id(self, folder_name, file_name):
+    def get_file_id(self, folder_name: str, file_name: str) -> str:
         folders = self._client.get('folder', {'text': folder_name})
-        assert len(folders) == 1
+        assert len(folders) == 1, f'Folder "{folder_name}" not found'
         folder_id = folders[0]['_id']
         iterator = self._client.listItem(folder_id, name=file_name)
         matches = list(iterator)
